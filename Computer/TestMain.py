78821682a414be3a48f674166bbde23a78439718
JustBeam <beamsvj@gmail.com>
2024-05-11 19:52:31 +0700

    Update2

--- Computer/TestMain.py
+++ Computer/TestMain.py
@@ -1,5 +1,17 @@
 import SocketCLI
+import threading
+import time
+from assestfunction import *
 SocketCLI.setup()
+
+
+from datetime import datetime
+import time
+timestamp = time.time()
+date_object = datetime.fromtimestamp(timestamp)
+print("Datetime:", date_object)
+
+
 
 data = {
      "int" : 9123748 ,
@@ -8,5 +20,8 @@ data = {
      "float" : 3.14 }
 }
 
-#while True:
-SocketCLI.data_send(data)
+while True:
+     the_data = SocketCLI.data_recv()
+     recv_data = bytestodic(the_data)
+     print(recv_data)
+#SocketCLI.data_send(data)
