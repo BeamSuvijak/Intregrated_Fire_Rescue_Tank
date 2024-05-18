a3a41f4de7147e64f6e0d6e8897ea29de0dc1f5b
Petch <petchingzaza@gmail.com>
2024-05-18 19:26:04 +0700

    MAIN

--- RasberryPi/main.py
+++ RasberryPi/main.py
@@ -4,12 +4,26 @@ import threading
 from datetime import datetime
 import time
 import operate
+import get_frame
 
 socketHOST.mainsocket.start()
+get_frame.setup()
+
+datasend = {}
+
+def SEND():
+    global datasend
+    while True:
+        frame = get_frame.fetch()
+        if frame:
+            datasend['frame'] = imgtotxt(frame)
+        """status"""
+        socketHOST.Conn.send(dictobytes(datasend))
 
 def RECV():
     data = socketHOST.chunkrecv(socketHOST.Conn)
     operate.run(data)
-
+SENDthread = threading.Thread(target=SEND)
 RECVthread = threading.Thread(target=RECV)
-RECVthread.run()
+RECVthread.start()
+SENDthread.start()
