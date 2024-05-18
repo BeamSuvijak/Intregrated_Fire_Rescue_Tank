3df2e093e839e66c11780d89be63ed9e2064ed2a
Petch <petchingzaza@gmail.com>
2024-05-18 17:27:52 +0700

    Aiming

--- Computer/Controlling.py
+++ Computer/Controlling.py
@@ -1,19 +1,23 @@
 import ControllingMethod
 
 method = 0
+PID = False
 
 """
 0 : Keyboard
 1 : Joystick
-2 : PID (auto)
 """
 
 def readJoy(): ControllingMethod.Joystick.packaging()
 def readKB(): ControllingMethod.KB.packaging()
-def readPID(): ControllingMethod.PID.packaging()
 
-usage = [readKB,readJoy,readPID]
+def readPID(): ControllingMethod.PID.packaging().get("Drive")
 
+usage = [readKB,readJoy]
 controlling_value = usage[method]
 
-if
+prvpid = False
+if(controlling_value["F9"] and controlling_value["F9"]!=prvpid): PID,prvpid = not PID,PID
+
+if(PID):
+    controlling_value["Drive"] = readPID()
